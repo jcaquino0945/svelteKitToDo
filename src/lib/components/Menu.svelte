03b3932e51af0de2b4a85c90f3f1e7d70ef1759d
Miguel Aquino <jcaquino0945@gmail.com>
2021-08-16 19:25:58 +0800

    toast component

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
     import { onMount } from 'svelte'
     import { toastService } from '$lib/services/toastService';
-	import { fade } from 'svelte/transition';
-
+    import Toast from './Toast.svelte';
     export let name;
     
     let currentDate = new Date();
     let notifPrompt = false;
+    let notif;
 
     onMount(() => {
         toastService.subscribe((message) => {
             //alert(message);
+            notif = message;
             setTimeout(function() {
                 notifPrompt = true;
                 setTimeout(function() {
@@ -22,21 +23,7 @@
 </script>
 
 {#if notifPrompt == true}
-<div class="notif-container" transition:fade>
-    <!-- Toast Notification Success-->
-    <div class="notif">
-        <!-- icons -->
-       <div class="notif-icons">
-         <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" class="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-           <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
-         </svg>
-       </div>
-       <!-- message -->
-       <div class="notif-text">
-         You have uploaded a comment!
-       </div>
-     </div>
-</div>
+<Toast notif={notif}></Toast>
 {/if}
 
 <div class="banner">
@@ -67,17 +54,5 @@
 }
 .banner-container {
     @apply container mx-auto h-full flex justify-end px-4  flex-col;
-}
-.notif-container {
-    @apply flex flex-col justify-center fixed w-full;
-}
-.notif {
-    @apply flex items-center bg-green-500 border-l-4 border-green-700 py-2 px-3 shadow-md mb-2;
-}
-.notif-icons {
-    @apply text-green-500 rounded-full bg-white mr-3;
-}
-.notif-text {
-    @apply text-white max-w-xs;
 }
 </style>
---
+++ src/lib/components/Toast.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { fade } from 'svelte/transition';
+
+    export let notif;
+</script>
+
+<div class="notif-container" transition:fade>
+    <!-- Toast Notification Success-->
+    <div class="notif">
+        <!-- icons -->
+       <div class="notif-icons">
+         <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" class="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+           <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+         </svg>
+       </div>
+       <!-- message -->
+       <div class="notif-text">
+         {notif}
+       </div>
+     </div>
+</div>
+
+<style lang="postcss">
+.notif-container {
+    @apply flex flex-col justify-center fixed w-full;
+}
+.notif {
+    @apply flex items-center bg-green-500 border-l-4 border-green-700 py-2 px-3 shadow-md mb-2;
+}
+.notif-icons {
+    @apply text-green-500 rounded-full bg-white mr-3;
+}
+.notif-text {
+    @apply text-white max-w-xs;
+}
+</style>
